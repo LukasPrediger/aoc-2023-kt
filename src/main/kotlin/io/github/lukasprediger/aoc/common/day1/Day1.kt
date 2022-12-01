20b32756ffcfeae910fa65057e6c7bbcac11c846
Lukas Prediger <lukas.prediger@rewe-digital.com>
2022-12-01 16:25:51 +0100

    Day1

--- src/main/kotlin/io/github/lukasprediger/aoc/common/day1/Day1.kt
+++ src/main/kotlin/io/github/lukasprediger/aoc/common/day1/Day1.kt
@@ -1,3 +1,36 @@
 package io.github.lukasprediger.aoc.common.day1
 
-fun main() {}
+import io.github.lukasprediger.aoc.common.readInput
+
+fun main() {
+    println(part1())
+    println(part2())
+}
+
+fun part1(): Int {
+    val elves = getElfCalories()
+
+    return elves.max()
+}
+
+fun part2(): Int {
+    val elves = getElfCalories()
+    return elves.sorted().takeLast(3).sum()
+}
+
+private fun getElfCalories(): MutableList<Int> {
+    val elves = mutableListOf<Int>()
+
+    val input = readInput(1)
+
+    var current = 0
+    input.forEach {
+        if (it.isBlank()) {
+            elves.add(current)
+            current = 0
+        } else {
+            current += it.toInt()
+        }
+    }
+    return elves
+}
