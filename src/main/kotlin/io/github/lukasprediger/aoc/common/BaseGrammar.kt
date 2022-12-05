c3ba0838aebaff71fd79d237cff581884c9cc249
Lukas Prediger <lukas.prediger@rewe-digital.com>
2022-12-05 21:30:29 +0100

    Day5

--- src/main/kotlin/io/github/lukasprediger/aoc/common/BaseGrammar.kt
+++ src/main/kotlin/io/github/lukasprediger/aoc/common/BaseGrammar.kt
@@ -12,7 +12,9 @@ import com.github.h0tk3y.betterParse.parser.parse
 abstract class BaseGrammar<T> : Grammar<T>() {
     protected val BLANK_LINE by regexToken("(?:\\h*\\n){2,}")
 
-    protected val CHAR by regexToken("\\w") map { it.value.text.first() }
+    private val charToken by regexToken("\\w")
+
+    protected val CHAR by charToken map { it.value.text.first() }
     protected val STRING by oneOrMore(CHAR) map { it.joinToString() }
 
     protected val DIGITS by regexToken("\\d*")
@@ -21,6 +23,11 @@ abstract class BaseGrammar<T> : Grammar<T>() {
     protected val MINUS by literalToken("-")
     protected val COMMA by literalToken(",")
 
+    protected val OPENING_BRACKET by literalToken("[")
+    protected val CLOSING_BRACKET by literalToken("]")
+
+    protected val SPACE by regexToken("\\s")
     protected val WHITESPACE by regexToken("\\s+", ignore = true)
+    
     fun parseString(input: String) = parse(tokenizer.tokenize(input))
 }
---
+++ src/main/kotlin/io/github/lukasprediger/aoc/days/day5/Day5.kt
@@ -0,0 +1,75 @@
+package io.github.lukasprediger.aoc.days.day5
+
+import com.github.h0tk3y.betterParse.combinators.*
+import com.github.h0tk3y.betterParse.grammar.Grammar
+import com.github.h0tk3y.betterParse.lexer.literalToken
+import com.github.h0tk3y.betterParse.lexer.regexToken
+import com.github.h0tk3y.betterParse.parser.Parser
+import com.github.h0tk3y.betterParse.parser.parse
+import io.github.lukasprediger.aoc.common.readInputRaw
+import javax.sql.RowSetEvent
+
+fun main() {
+    val input = readInputRaw(5)
+    println(part1(input))
+    println(part2(input))
+}
+
+fun part1(input: String): String = process(input, true)
+fun part2(input: String): String = process(input, false)
+
+fun process(input: String, reverseOrder: Boolean): String {
+    val (cratesString, operationsString) = input.split("\n\n")
+    var crates = cratesString.lines()
+        .dropLast(1) // dropping numbers
+        .map { line ->
+            line.chunked(4)
+                .map { if(it.length == 4) it.dropLast(1) else it }
+                .map { if (it == "   ") null else it[1] }
+        }.let {
+            List(it.first().size) { index ->
+                it.mapNotNull { row -> row[index] }
+            }
+        }.map(List<Char>::reversed)
+
+    val operations = OperationGrammar.parseString(operationsString)
+
+    operations.forEach {
+        crates = it.process(crates, reverseOrder)
+    }
+
+    return crates.joinToString("") { it.last().toString() }
+}
+
+
+data class Operation(val amount: Int, val from: Int, val to: Int) {
+    fun process(crates: List<List<Char>>, reverseOrder:Boolean = true): List<List<Char>> =
+        crates.toMutableList().also {
+            it[to] = it[to] + it[from].takeLast(amount).let { if(reverseOrder) it.reversed() else it }
+            it[from] = it[from].dropLast(amount)
+        }
+}
+
+object OperationGrammar : Grammar<List<Operation>>() {
+    val moveToken by literalToken("move")
+    val fromToken by literalToken("from")
+    val toToken by literalToken("to")
+
+    val WHITESPACE by regexToken("\\s+", ignore = true)
+
+    val DIGITS by regexToken("\\d*")
+    val INTEGER by DIGITS use { text.toInt() }
+
+
+    var operation: Parser<Operation> =
+        skip(moveToken) and INTEGER and skip(fromToken) and INTEGER and skip(toToken) and INTEGER map { (amount, from, to) ->
+            Operation(amount, from - 1, to - 1)
+        }
+
+    val operations by separatedTerms(operation, WHITESPACE)
+
+    override val rootParser = operations
+
+}
+
+fun <T> Grammar<T>.parseString(input: String) = parse(tokenizer.tokenize(input))
